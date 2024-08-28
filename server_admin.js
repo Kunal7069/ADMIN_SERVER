f1fe3e460c1fc83ea07290ded408bdce501c44dc
Kunal7069 <107999879+Kunal7069@users.noreply.github.com>
2024-08-29 01:29:04 +0530

    Update server_admin.js

--- server_admin.js
+++ server_admin.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors(
   {
-    origin: 'http://localhost:3000', // Your frontend URL
+    origin: '*', // Your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   }
